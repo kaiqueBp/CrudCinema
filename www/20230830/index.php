862d7942c3e00ddcb69007ce9d21da373a0d83a9
felipepperez <fpproducoes@gmail.com>
2023-09-04 21:07:55 -0400

    Aula 04/09/2023

---
+++ www/20230830/index.php
@@ -0,0 +1,18 @@
+<?php
+setcookie("cookie", "Felipe", time() + (60 * 60 * 24));
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>O cookie foi inicializado.</h1>
+</body>
+
+</html>
